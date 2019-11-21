52ac1f32bc246ea71531e67f5b0abb7e34874b25
user <user@LAPTOP-IVGHLFS4>
2019-11-21 22:06:20 +0800

    11/21

--- HelloJava/src/test1.java
+++ HelloJava/src/test1.java
@@ -1,22 +1,22 @@
 
 public class test1 {
-    public static void firstMethod () {
-        System.out.println("Method 1");
-    }
     
-    public static void main(String[] args) {
-        int a = 1, b = 2, c = 0;
-        if ( a == 1 && b ==2 && c == 1) {
-            System.out.println("case 1");
-        } else if ( a == 0 || b == 2) {
-            System.out.println("case 2");
+    static void hello() {
+        for (int i = 1; i < 11; i++) {
+            System.out.print(i);
+            for (int j = 1; j < 10; j++) {
+                for (int k = 1; k < 10; k++) {
+                    System.out.print(j+"*"+k+" ");                    
+                }
+                System.out.println(" ");                               
+            }
         }
-        int z = 0;
-        for (int i = 1 ; i < 11; i++) {
-            z = z + i;
-        }
-        System.out.println("total :" + z);
-        
-        
+        System.out.println("hi, in hello()");
+        System.out.println("bye,in hello()");
     }
+    public static void main(String[] args) {
+        
+        
+hello();                                                                         //呼叫口號       
+    }       
 }
