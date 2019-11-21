efb9bc40b6f53940d016f577cf944e28aa71df7a
user <user@LAPTOP-IVGHLFS4>
2019-11-21 22:13:17 +0800

    11/21 (2)

--- HelloJava/src/test1.java
+++ HelloJava/src/test1.java
@@ -2,21 +2,24 @@
 public class test1 {
     
     static void hello() {
+        System.out.println("hi,APPX");
         for (int i = 1; i < 11; i++) {
             System.out.print(i);
+        }
             for (int j = 1; j < 10; j++) {
                 for (int k = 1; k < 10; k++) {
                     System.out.print(j+"*"+k+" ");                    
                 }
                 System.out.println(" ");                               
             }
-        }
-        System.out.println("hi, in hello()");
-        System.out.println("bye,in hello()");
+        
+        System.out.println("hi, APPX");
     }
     public static void main(String[] args) {
         
         
 hello();                                                                         //呼叫口號       
-    }       
+    
+    }
+    
 }
