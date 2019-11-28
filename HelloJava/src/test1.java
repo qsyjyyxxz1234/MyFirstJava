01a9c64060724ca69158085793b708de362e5539
user <user@LAPTOP-IVGHLFS4>
2019-11-28 17:00:07 +0800

    11/26

--- HelloJava/src/test1.java
+++ HelloJava/src/test1.java
@@ -1,25 +1,34 @@
 
 public class test1 {
-    
-    static void hello() {
-        System.out.println("hi,APPX");
-        for (int i = 1; i < 11; i++) {
-            System.out.print(i);
+
+    static int hello(int a, int b, char c) {                                     //定義口號
+        int i = 20;
+        int total = 0;
+        for (int j = a; j <= b; j++) {
+            System.out.print(c + " ");
+            total = total + j;
         }
-            for (int j = 1; j < 10; j++) {
-                for (int k = 1; k < 10; k++) {
-                    System.out.print(j+"*"+k+" ");                    
-                }
-                System.out.println(" ");                               
-            }
-        
-        System.out.println("hi, APPX");
+        System.out.println("in result is ,total =" + total);
+        return total;
     }
+
     public static void main(String[] args) {
-        
-        
-hello();                                                                         //呼叫口號       
-    
+
+        int result = hello(2, 8, '*');                                            //呼叫口號       
+        hello(3, 7, '!');
+        double d = Math.random();
+        System.out.println("d =" + d++ * 1000 % 42 + 1);
+        int[] data = new int[6];
+        for (int o = 1; o < 6; o++) {                                            //.length
+            data[o] = ((int) (Math.random() * 100)) % 42 + 1;
+            System.out.println("data[" + o + "] =" + data[o]);
+        }
+        //data[0] = ((int) (Math.random() * 100)) % 42 + 1;
+        //data[1] = ((int) (Math.random() * 100)) % 42 + 1;
+        //data[2] = ((int) (Math.random() * 100)) % 42 + 1;
+        //data[3] = ((int) (Math.random() * 100)) % 42 + 1;
+        //data[4] = ((int) (Math.random() * 100)) % 42 + 1;
+        //data[5] = ((int) (Math.random() * 100)) % 42 + 1;
+
     }
-    
 }
