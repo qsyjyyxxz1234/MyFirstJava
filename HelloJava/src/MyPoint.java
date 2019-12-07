ff06ed886a36a6f82713e31548924772f4b51db6
user <user@LAPTOP-IVGHLFS4>
2019-12-07 17:10:47 +0800

    12/7 4 17:10

--- HelloJava/src/MyPoint.java
+++ HelloJava/src/MyPoint.java
@@ -1,12 +1,13 @@
- public class MyPoint {
 
-    private int x;                                      //private(只能在同一類別內,若超出範圍則不能存取)
-    private int y;                                      //可加在成員變數、涵式與( *類別(class) )上
+public class MyPoint {
 
-    public MyPoint(int i, int j) {                                  //constructor建構子(製造/產生物件的涵式)(沒有回傳值)(名稱跟類別名稱一致)
+    private int x;                                                  //private(只能在同一類別內,若超出範圍則不能存取)
+    private int y;                                                  //可加在成員變數、涵式與( *類別(class) )上
+
+    public MyPoint(int x, int y) {                                  //constructor建構子(製造/產生物件的涵式)(沒有回傳值)(名稱跟類別名稱一致)
         System.out.println("in MyPoint()");
-        x = i;
-        y = j;
+        this.x = x;                                                 //this(只能用在涵式裡面/關鍵字)(正在執行程式中的物件)
+        this.y = y;
         //print();
     }
 
@@ -27,7 +28,7 @@
     }
 
     public void print() {
-        System.out.println("(x,y)@(" + x + "," + y + ")");
+        System.out.println("(x,y)@(" + this.x + "," + this.y + ")");
     }
 
 }
