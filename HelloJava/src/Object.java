394bc597dc434cf72d5fa51017caba0f0f20e5f2
user <user@LAPTOP-IVGHLFS4>
2019-12-07 12:58:07 +0800

    12/7 2

--- HelloJava/src/Object.java
+++ HelloJava/src/Object.java
@@ -18,30 +18,35 @@ class car {
 public class Object {
 
     public static void main(String[] args) {
-        car c1 = new car();     //car=類別名 c1=變數名 //car c1(第一個物件) car c2(第二個物件)
-        c1.brand = "Toyota";
-        c1.price = 750000;
-        c1.volume = 3000;
-        System.out.println("c1.brand = " + c1.brand);
-        System.out.println("c1.price = " + c1.price + "$");
-        System.out.println("c1.volume = " + c1.volume + "c.c.");
-        System.out.println("c1.speed = " + c1.speed);
-        c1.speedup();
-        c1.speedup();
-        System.out.println("c1.speed = " + c1.speed);
-        c1.slowdown();
-        c1.slowdown();
-        System.out.println("c1.speed = " + c1.speed);
+        MyPoint p1 = new MyPoint();
+        p1.setX(12);
+        p1.setY(7);
+        //System.out.println("p1.x " + p1.getX());
+        //System.out.println("p1.y " + p1.getY());
+        p1.print();
 
-        car c2 = new car();
-        c2.brand = "Suzuki";
-        c2.price = 780000;
-        c2.volume = 3000;
-        System.out.println("c2.brand = " + c2.brand);
-        System.out.println("c2.price = " + c2.price + "$");
-        System.out.println("c2.volume = " + c2.volume + "c.c.");
-        System.out.println("c2.speed = " + c2.speed);
-
+        //car c1 = new car();     //car=類別名 c1=變數名 //car c1(第一個物件) car c2(第二個物件)
+        //c1.brand = "Toyota";
+        //c1.price = 750000;
+        //c1.volume = 3000;
+        //System.out.println("c1.brand = " + c1.brand);
+        //System.out.println("c1.price = " + c1.price + "$");
+        //System.out.println("c1.volume = " + c1.volume + "c.c.");
+        //System.out.println("c1.speed = " + c1.speed);
+        //c1.speedup();
+        //c1.speedup();
+        //System.out.println("c1.speed = " + c1.speed);
+        //c1.slowdown();
+        //c1.slowdown();
+        //System.out.println("c1.speed = " + c1.speed);
+        //car c2 = new car();
+        //c2.brand = "Suzuki";
+        //c2.price = 780000;
+        //c2.volume = 3000;
+        //System.out.println("c2.brand = " + c2.brand);
+        //System.out.println("c2.price = " + c2.price + "$");
+        //System.out.println("c2.volume = " + c2.volume + "c.c.");
+        //System.out.println("c2.speed = " + c2.speed);
     }
 
 }
